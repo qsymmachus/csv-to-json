6f39af3dc327408cf603c36214008de3bad2f151
Justin M. <justinm715@gmail.com>
2014-06-04 23:37:20 -0700

    Update csv_to_json.rb

--- csv_to_json.rb
+++ csv_to_json.rb
@@ -11,7 +11,7 @@ module CSVtoJSON
     rows = convert_booleans(rows)
 
     keys = rows.shift
-    # mark null  headers that begin with pound sign (#)
+    # mark null the headers that begin with pound sign (#)
     # ['#a','#b','#c','e','f','g'] becomes [nil,nil,nil,3,4,5]
     keys_nulled = keys.map.with_index(0) { |e,i| e.match(/\A\#.*/) ? nil : i }
 
