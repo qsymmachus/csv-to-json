5482d03027efdfbea6db6e90482ffe164d384fdb
Justin Martinez <justinm715@gmail.com>
2014-06-04 23:01:24 -0700

    add ability to ignore headers/columns that begin with #

--- csv_to_json.rb
+++ csv_to_json.rb
@@ -2,14 +2,23 @@ require 'csv'
 require 'json'
 
 module CSVtoJSON
+
+  # Take a CSV file and spit out a JSON file
+  #
+  # If a column's header begins with a pound sign (#), the entire column is ignored  
   def self.convert_csv(input, output)
     rows = CSV.open(input).read
     rows = convert_booleans(rows)
+
+    # nullify headers that begin with pound sign (#)
+    # ['#a','#b','#c','e','f','g'] becomes [nil,nil,nil,'e','f','g']
     keys = rows.shift
+    keys_nulled = keys.map.with_index(0) { |e,i| e.match(/\A\#.*/) ? nil : i }
 
     File.open(output, 'w') do |file|
       data = rows.map do |values|
-        Hash[keys.zip(values)]
+        # ignore values whose header was nullified
+        Hash[keys_nulled.zip(keys,values).map{|f| f[0].nil? ? nil : f[1,2]}.compact ]
       end
       file.write(JSON.pretty_generate data)
     end
